7828053784060c0083c059a0081411e81f7b1b37
arooly <alex@arooly.com>
2022-05-11 14:46:52 -0700

    Work-04

--- src/components/NotificationsBell/NotificationsBell.js
+++ src/components/NotificationsBell/NotificationsBell.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import { Badge, Tooltip, IconButton } from "@mui/material";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import BasicMenu from "../../components/common/BasicMenu/BasicMenu";
 
-const NotificationsBell = ({ iconColor, badgeContent, onClick }) => {
+const NotificationsBell = ({ iconColor, badgeContent }) => {
   const newNotifications = `You have ${badgeContent} new notifications`;
   const noNotifications = "You have no notifications";
+
+  const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = (event) => {
+    setOpen(false);
+  };
+
   return (
     <div>
       <Tooltip title={badgeContent ? newNotifications : noNotifications}>
--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,31 +1,13 @@
 import React from "react";
-import { useState } from "react";
 import Grid from "@mui/material/Grid";
 import NotificationsBell from "../../components/NotificationsBell/NotificationsBell";
 import BasicMenu from "../../components/common/BasicMenu/BasicMenu";
 
 const Authentication = () => {
-  const [open, setOpen] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handleOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-    setOpen(true);
-  };
-
-  const handleClose = (event) => {
-    setOpen(false);
-  };
-
   return (
     <Grid item xs={8}>
-      <NotificationsBell
-        iconColor={"primary"}
-        badgeContent={0}
-        anchorEl={anchorEl}
-        onClick={handleOpen}
-      />
-      <BasicMenu open={open} anchorEl={anchorEl} handleClose={handleClose} />
+      <NotificationsBell iconColor={"primary"} badgeContent={0} />
+      <BasicMenu />
     </Grid>
   );
 };
